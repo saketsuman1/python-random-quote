5f478574ed19246aebb7cd3818db0c0e24847b25
saket suman <saket1@debian10.saket.com>
2021-08-05 02:44:26 -0400

    Random quote.

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import random
 
 def main():
-	print("Keep it logically awesome.")
-	print("Saket")
-	#f = open("quotes.txt")
-	#quotes = f.readlines()
-	#f.close()
-
-	#print(quotes)
+	#print("Keep it logically awesome.")
+	#print("Saket")
+	f = open("quotes.txt")
+	quotes = f.readlines()
+	f.close()
+	#print(len(quotes))
+	last = 13
+	rnd = random.randint(0,last)
+	print(quotes[rnd])
 
 if __name__ == "__main__":
 	main()
